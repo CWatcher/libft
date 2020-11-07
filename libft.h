cddb48b3f8ac4341eb8e850d1c06004bafee61d4
Carmine Watcher <CWatcher@student.21-school.ru>
2020-11-07 13:43:41 +0300

    += atoi(), strdup(). Makefile += so

--- libft.h
+++ libft.h
@@ -18,5 +18,8 @@
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s);
+int		ft_atoi(char *str);
+char	**ft_split(char const *s, char c);
 
 #endif
