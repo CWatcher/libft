bf11e7f43fb39c24bcf6e1ae96c110f5cb79cefb
Carmine Watcher <cwatcher@21-school.ru>
2020-11-08 12:31:31 +0300

    if (dst != src)

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/05 15:40:17 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/11/05 17:52:07 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/11/08 12:29:26 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,17 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 	const unsigned char	*s;
 	size_t				i;
 
-	d = dst;
-	s = src;
-	i = 0;
-	while (i < n)
+	if (dst != src)
 	{
-		if ((d[i] = s[i]) == (unsigned char)c)
-			return (d + i + 1);
-		i++;
+		d = dst;
+		s = src;
+		i = 0;
+		while (i < n)
+		{
+			if ((d[i] = s[i]) == (unsigned char)c)
+				return (d + i + 1);
+			i++;
+		}
 	}
 	return (NULL);
 }
