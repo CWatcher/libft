5b83325583e95360be1c4e11b26332cf803215dd
Carmine Watcher <cwatcher@21-school.ru>
2020-11-12 12:52:50 +0300

    [squash] += substr

--- libft.h
+++ libft.h
@@ -41,6 +41,7 @@ int		ft_isascii(int c);
 int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
