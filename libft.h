3f515624aef176dad2aca2a8ab0739bbc6ae7953
Carmine Watcher <cwatcher@21-school.ru>
2020-11-12 12:48:47 +0300

    [squash] += strtrim

--- libft.h
+++ libft.h
@@ -27,6 +27,7 @@ size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strdup(const char *s);
+char	*ft_strndup(const char *s, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *s, const char *s2, size_t n);
@@ -40,6 +41,7 @@ int		ft_isprint(int c);
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
 
 #endif
