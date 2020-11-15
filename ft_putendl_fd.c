3f13d4bb78016a82b9ab633ceb33ddd69d4011ba
Carmine Watcher <cwatcher@21-school.ru>
2020-11-15 20:54:09 +0300

    += put* (part2)

---
+++ ft_putendl_fd.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/14 11:45:31 by CWatcher          #+#    #+#             */
+/*   Updated: 2020/11/15 20:49:13 by CWatcher         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	write(fd, s, ft_strlen(s) * sizeof(*s));
+	write(fd, "\n", sizeof(char));
+}
