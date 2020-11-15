4c4cccc0a8396c1c56a68edfa67504ffdd3839d5
Carmine Watcher <cwatcher@21-school.ru>
2020-11-15 17:33:44 +0300

    [norm]

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 12:50:55 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/11/15 17:06:41 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/11/15 17:32:50 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*pl;
 
-	if (!(pl = malloc(sizeof (*pl))))
+	if (!(pl = malloc(sizeof(*pl))))
 		return (NULL);
 	pl->content = content;
 	return (pl);
