a2827fd85e7e79169456816552d70d2b1d8517f6
Carmine Watcher <cwatcher@21-school.ru>
2020-11-16 12:49:21 +0300

    [done] putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -1,19 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/14 11:45:31 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/11/15 19:58:29 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/11/16 12:43:41 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "libft.h"
 
-void	ft_putstr_fd(char *s, int fd)
+void	ft_putnbr_fd(int n, int fd)
 {
-	write(fd, s, ft_strlen(s) * sizeof(*s));
+	unsigned	num;
+	char		r;
+
+	if (n < 0)
+		write(fd, "-", sizeof(char));
+	num = n < 0 ? -n : n;
+	if (num > 9)
+		ft_putnbr_fd(num / 10, fd);
+	r = num % 10 + '0';
+	write(fd, &r, sizeof(r));
 }
