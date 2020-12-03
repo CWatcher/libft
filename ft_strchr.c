0eb2daedc0075b5f65b08a6bfe947e3448a3b695
CWatcher <cwatcher@student.21-school.ru>
2020-12-03 23:00:18 +0300

    (char)c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/08 17:17:41 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/11/19 16:30:12 by CWatcher         ###   ########.fr       */
+/*   Updated: 2020/12/03 21:19:12 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (s && *s != c)
+	while (s && *s != (char)c)
 		if (!*s++)
 			return (NULL);
 	return ((char *)s);
