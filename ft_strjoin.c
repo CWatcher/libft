f32e7af259eef65f1bfb1b2adfbfbd0ed8e7b9dd
Carmine Watcher <cwatcher@student.21-school.ru>
2021-08-18 20:46:21 +0300

    [norm]3 ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: CWatcher <cwatcher@student.21-school.r>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/11 10:32:52 by CWatcher          #+#    #+#             */
-/*   Updated: 2020/12/20 17:32:04 by CWatcher         ###   ########.fr       */
+/*   Updated: 2021/07/21 17:56:35 by CWatcher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char *s0;
-	char *s;
+	char	*s0;
+	char	*s;
 
-	if ((!s1 && !s2)
-		|| !(s0 = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char))))
+	if (!s1 && !s2)
+		return (NULL);
+	s0 = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!s0)
 		return (NULL);
 	s = s0;
 	while (s1 && *s1)
